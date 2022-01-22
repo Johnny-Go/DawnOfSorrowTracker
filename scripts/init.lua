742b14a5be4c43096eec838cb990970c90ac9ebf
Johnny-Go <49007932+Johnny-Go@users.noreply.github.com>
2022-01-21 19:38:31 -0800

    Adding map images, Standard tracker, tabs

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,11 +3,11 @@ ScriptHost:LoadScript("scripts/settings.lua")
 
 Tracker:AddItems("items/items.json")
 
---if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
---  Tracker:AddMaps("maps/maps.json")
---  Tracker:AddLocations("locations/locations.json")
---  Tracker.DisplayAllLocations = PREFERENCE_DISPLAY_ALL_LOCATIONS
---end
+if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
+  Tracker:AddMaps("maps/maps.json")
+  Tracker:AddLocations("locations/locations.json")
+  Tracker.DisplayAllLocations = PREFERENCE_DISPLAY_ALL_LOCATIONS
+end
 
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/tracker.json")
