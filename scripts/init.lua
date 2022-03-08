d464b84c3bd17f93334058f4c039b8e586d1721f
Johnny-Go <JohnnyGo176@gmail.com>
2022-03-08 15:20:55 -0800

    Adding broadcast view, and updating manifest details

--- scripts/init.lua
+++ scripts/init.lua
@@ -14,4 +14,4 @@ end
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/tracker.json")
 
-Tracker:AddLayouts("layouts/standard_broadcast.json")
+Tracker:AddLayouts("layouts/broadcast.json")
